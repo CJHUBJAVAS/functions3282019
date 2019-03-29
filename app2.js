6c0e426c17c2c1d238709cdcb61839e46803e2a3
h22121 <h22121@lab.cpcc.edu>
2019-03-28 20:47:00 -0400

    846pm classwork

--- app2.js
+++ app2.js
@@ -1,14 +1,25 @@
+var name = "Other Bruce";
+
 const bruce = {
     name: "Bruce",
-    greet: function() {
+    bruceGreet: function() {
+        console.log(name);
+        this.name = "This Bruce";
         return `Hello ${this.name}`;
     }
 };
 
-console.log(bruce.greet());
+console.log(bruce.bruceGreet());
+
+var bruceCopy = bruce;
+var x = 10;
+var y = x;
+y = 11;
+
+bruceCopy.name = "BigBruce";
+
 
 const madeline = {name: "Madeline"};
-
 function greet() {
     return `Hello ${this.name}`;
 }
